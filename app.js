c93a40eb97e1259dedeb79a551c4b1d6b78faa7a
Ian Avery <ian.avery@freightvana.io>
2021-06-15 15:08:07 -0700

    updated tests

--- app.js
+++ app.js
@@ -6,6 +6,34 @@ app.get('/hello', (req, res) => {
 	res.status(200).send('hello world');
 });
 
-app.listen(3001, () => {
-	console.log('server listening on port 3001');
-});
+const runServer = async (portNumber) => {
+	try {
+		server = app.listen(portNumber, () => {
+			console.log(`Server listening on port ${portNumber}`);
+		});
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const closeServer = () => {
+	try {
+		server.close();
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+if (require.main === module) {
+	try {
+		runServer(3000);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+module.exports = {
+	app,
+	runServer,
+	closeServer,
+};
--- test/test.js
+++ test/test.js
@@ -1,8 +1,30 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+const should = chai.should();
+
+chai.use(chaiHTTP);
+
+const {
+	app,
+	runServer,
+	closeServer,
+} = require('../app');
+
+describe('circle ci poc', () => {
+	before(() => {
+		return runServer();
+	});
+	after(() => {
+		return closeServer();
+	});
+	describe('GET endpoint', () => {
+		it('should return a string', (done) => {
+			chai.request(app)
+				.get('/hello')
+				.end((err, res) => {
+					res.should.have.status(200);
+					done();
+				});
+		});
+	});
 });
